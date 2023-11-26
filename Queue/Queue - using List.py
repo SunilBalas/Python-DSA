892664dbe718b876cfa9e05940a52057dc59f7ca
SunilBalas <balassunil606@gmail.com>
2023-11-26 10:18:39 +0530

    Refactored the Queue Data Structure using List

--- Queue/Queue - using List.py
+++ Queue/Queue - using List.py
@@ -1,50 +1,54 @@
 from typing import Any
 
 class Queue:
-    def __init__(self, front=None, rear=None) -> None:
+    def __init__(self) -> None:
         self.items = []
-        self.front = front
-        self.rear = rear
     
     def is_empty(self) -> bool:
         return len(self.items) == 0
     
     def enqueue(self, data) -> None:
         self.items.append(data)
-        if len(self.items) == 1:
-            self.rear = self.front = data
-        else:
-            self.rear = data
     
-    def dequeue(self) -> Any:
+    def dequeue(self) -> None:
         if not self.is_empty():
-            data = self.front
-            self.items.remove(self.front)
-            if len(self.items) > 0:
-                self.front = self.items[0]
-            else:
-                self.front = self.rear = None
-            return data
+            self.items.remove(self.items[-1])
         else:
             raise IndexError("Queue is Empty.")
     
     def get_front(self) -> Any:
-        return self.front
+        if not self.is_empty():
+            return self.items[0] 
+        else:
+            raise IndexError("Queue is Empty.")
     
     def get_rear(self) -> Any:
-        return self.rear
+        if not self.is_empty():
+            return self.items[-1] 
+        else:
+            raise IndexError("Queue is Empty.")
     
     def size(self) -> int:
         return len(self.items)
 
 q = Queue()
+
+try:
+    q.get_front()
+except IndexError as ex:
+    print(ex.args[0])
+
 q.enqueue(10)
 q.enqueue(20)
 q.enqueue(30)
 q.enqueue(40)
 q.enqueue(50)
 
-print(f"Removed Element: {q.dequeue()}")
+try:
+    q.dequeue()
+except IndexError as ex:
+    print(ex.args[0])
+    
 print(f"Front Element: {q.get_front()}")
 print(f"Rear Element: {q.get_rear()}")
 print(f"Queue Size: {q.size()}")
