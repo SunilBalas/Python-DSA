9c106804159d78ca37d8801ef025f64103b177fe
SunilBalas <balassunil606@gmail.com>
2023-11-26 11:46:04 +0530

    Implemented Queue Data Structure using Extending List, LinkedList, Extending LinkedList and LinkedList Concept

---
+++ Queue/Queue - using Extending List.py
@@ -0,0 +1,59 @@
+from typing import Any
+
+class Queue(list):
+    def is_empty(self) -> bool:
+        return len(self) == 0
+    
+    def enqueue(self, data) -> None:
+        self.append(data)
+    
+    def dequeue(self) -> None:
+        if not self.is_empty():
+            self.remove(self[0])
+        else:
+            raise IndexError("Queue is Empty.")
+    
+    def get_front(self) -> Any:
+        if not self.is_empty():
+            return self[0] 
+        else:
+            raise IndexError("Queue is Empty.")
+    
+    def get_rear(self) -> Any:
+        if not self.is_empty():
+            return self[-1] 
+        else:
+            raise IndexError("Queue is Empty.")
+    
+    def size(self) -> int:
+        return len(self)
+    
+    def insert(self, index, data) -> None:
+        raise AttributeError("No attribute 'insert' in Queue.")
+
+q = Queue()
+
+try:
+    q.get_front()
+except IndexError as ex:
+    print(ex.args[0])
+
+q.enqueue(10)
+q.enqueue(20)
+q.enqueue(30)
+q.enqueue(40)
+q.enqueue(50)
+
+try:
+    q.dequeue()
+except IndexError as ex:
+    print(ex.args[0])
+    
+print(f"Front Element: {q.get_front()}")
+print(f"Rear Element: {q.get_rear()}")
+print(f"Queue Size: {q.size()}")
+
+try:
+    q.insert(0, -10)
+except AttributeError as ex:
+    print(ex.args[0])
