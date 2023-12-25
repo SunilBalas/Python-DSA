f604dbb784517fa9928e4e53947e8134d071649b
SunilBalas <balassunil606@gmail.com>
2023-12-25 21:49:10 +0530

    Changed the loop range in Selection Sort and implemented the Insertion Sort Algorithm

---
+++ Sorting/Insertion_Sort.py
@@ -0,0 +1,16 @@
+from typing import List
+
+def insertion_sort(elements:list) -> None:
+    n = len(elements)
+    for i in range(n):
+        temp = elements[i]
+        
+        j = i-1
+        while j >= 0 and temp < elements[j]:
+            elements[j+1] = elements[j]
+            j -= 1
+        elements[j+1] = temp
+
+elements = [20, 50, 63, 19, 8, -2, 98]
+insertion_sort(elements)
+print(elements)
--- Sorting/Selection_Sort.py
+++ Sorting/Selection_Sort.py
@@ -2,7 +2,7 @@ from typing import List
 
 def selection_sort(elements:List) -> None:
     n = len(elements)
-    for i in range(n):
+    for i in range(n-1):
         min = i
         for j in range(i+1, n):
             if elements[min] > elements[j]:
